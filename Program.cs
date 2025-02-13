cf623ab230ffc9968e22dd246e62919aa96b9933
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-02-13 16:44:20 -0700

    :sparkles: Important Updates
    Renamed DockerProject-CPSY300 to DockerProject_CPSY300
    
    Updated .gitignore
    
    Added a lock.json file to keep track of package versions.
    
    And more!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 using DockerProject_CPSY300.Components;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddBlazorise(options => {
+        options.Immediate = true;
+    })
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -9,9 +19,8 @@ builder.Services.AddRazorComponents()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
