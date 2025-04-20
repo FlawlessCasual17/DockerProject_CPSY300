7b7a34624f75ea12825743aca02dd2b872e674e2
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-19 18:13:24 -0600

    Update `WebApp/src/components/AddDataForm.tsx`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- WebApp/src/components/AddDataForm.tsx
+++ WebApp/src/components/AddDataForm.tsx
@@ -39,9 +39,6 @@ export default function AddDataForm() {
     function handleSubmit(event: SubmitEvent) {
         event.preventDefault();
 
-        if (Object.values(studentData()).every(v => v === null))
-            console.log('Either all or some properties in `studentData()` were not filled');
-
         (async () => {
             try {
                 const response = await fetch('http://127.0.0.1:8080/student', {
